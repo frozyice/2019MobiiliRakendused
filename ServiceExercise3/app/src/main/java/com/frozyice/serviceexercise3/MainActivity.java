4bcf6166a952993e2a727086cda096304f1ab17d
Karmo <Karmo.Kiima@Eesti.ee>
2019-11-07 13:17:00 +0200

    ServiceEx3 Add activity_main layout Add RingService Add methods to MainActivity

--- ServiceExercise3/app/src/main/java/com/frozyice/serviceexercise3/MainActivity.java
+++ ServiceExercise3/app/src/main/java/com/frozyice/serviceexercise3/MainActivity.java
@@ -2,13 +2,66 @@ package com.frozyice.serviceexercise3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent intent;
+    private Timer timer = new Timer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intent = new Intent(this, RingService.class);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        this.timerHandler();
+    }
+
+    private void timerHandler() {
+        final Handler handler = new Handler();
+
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask, 10000);
+
+    }
+
+    public void onBtnStart(View view) {
+
+        startService(intent);
+    }
+
+    public void onBtnStop(View view) {
+        //Intent intent = new Intent(this, RingService.class);
+        stopService(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (timer.equals(true)) timer.cancel();
+
     }
 }
---
+++ ServiceExercise3/app/src/main/java/com/frozyice/serviceexercise3/RingService.java
@@ -0,0 +1,32 @@
+package com.frozyice.serviceexercise3;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+
+
+
+public class RingService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.stop();
+    }
+}
